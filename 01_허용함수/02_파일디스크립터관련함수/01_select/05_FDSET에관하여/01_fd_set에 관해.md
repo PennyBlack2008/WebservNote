1700ecf6b7ccefbd063caa63ca1fbe3baf0d2019
Jinwoo Kang <jikang@student.42seoul.kr>
2021-04-17 22:02:49 +0900

    20210417 night push

--- 01_허용함수/02_파일디스크립터관련함수/01_select/05_FDSET에관하여/01_fd_set에 관해.md
+++ 01_허용함수/02_파일디스크립터관련함수/01_select/05_FDSET에관하여/01_fd_set에 관해.md
@@ -13,15 +13,25 @@ fd_set
 
  32 bit computer 는 fd 를 int data[2]; 로 저장 (Mac은 4바이트)
   -> fds_bits 의 크기(128 bytes)
- 
+
  ## 3. fd_set
  - __FD_SETSIZE
  	- 상수 : 1024
- 
+
  - __fd_mask
 	- 4 bytes 의 type
 
  - __NFDBITS
 	- __fd_mask 가 8 개. 32 bytes.
 
-## 4. fd_zero
+## 4. FD_ZERO
+ fd_set 으로 선언된 변수를 초기화할 때 사용
+
+## 5. FD_SET
+ 파일 디스크립터를 세팅해주는 방식은 비트 "|" 연산을 통해 해당 bit 의 값을 1로 바꿔준다.
+
+## 6. FD_CLR
+ 해당 파일 디스크립터를 "&= ~(1 << (d%32))" 연산을 통해 해당 bit 의 값을 0으로 바꿔준다.
+
+## 7. FD_ISSET
+ fd_set 안에서 해당 파일 디스크립터가 1인지 확인해주고 1이면 1을 반환, 아니면 0
--- 03_소켓강의유튜브/06_AdvancedMultipleClients.c
+++ 03_소켓강의유튜브/06_AdvancedMultipleClients.c
@@ -1,4 +1,23 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+// #include <sys/types.h>
+#include <netinet/in.h> // strcut sockaddr_in
+
 int main(void)
 {
-	
+	int server_sockfd, client_sockfd;
+	int server_len, client_len;
+	struct sockaddr_in server_address, client_address;
+	int result;
+	fd_set readfds, testfds;
+
+	server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_address.sin_port = htons(9734);
+	server_len = sizeof(server_address);
+	bind(server_sockfd, (struct sockaddr*)&server_address, server_len);
+	listen(server_sockfd, 5);
 }
