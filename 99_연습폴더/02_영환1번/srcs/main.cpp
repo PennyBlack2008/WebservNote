e666e5c543defd919ebcfe10c6aacf9030c4e0a7
PennyBlack2008 <jikang@student.42seoul.kr>
2021-04-27 12:37:43 +0900

    Feat: page reload, file transter

--- 99_연습폴더/02_영환1번/srcs/main.cpp
+++ 99_연습폴더/02_영환1번/srcs/main.cpp
@@ -1,26 +1,27 @@
 #include "Socket.hpp"
 #include "HttpMessage/HttpMessageRequest.hpp"
 #include "HttpMessage/HttpMessageResponse.hpp"
-
+#include <string>
 #include <string.h>	//	FIXME	나중에 지우기 테스트용 for memset
 
 // #define BUFFER_SIZE 65536
-#define BUFFER_SIZE 1000
+#define BUFFER_SIZE 100
 int main()
 {
 	// Create a socket (IPv4, TCP)
 	Socket socket(80, INADDR_ANY); // Listen 작업까지 되어 있는 소켓
 	size_t connections;
-	int bytesRead = BUFFER_SIZE - 1;
+	int bytesRead = BUFFER_SIZE;
 	char buffer[BUFFER_SIZE];
 	while (1)
 	{
-		bytesRead = BUFFER_SIZE - 1;
+		std::string	request_buffer = "";
+		bytesRead = BUFFER_SIZE;
 		connections = socket.Accept(connections);
 
 		/* 테스트 진행 */
 		//	STUB : Read from the connection
-		while (bytesRead == BUFFER_SIZE - 1)
+		while (bytesRead == BUFFER_SIZE)
 		{
 			memset(buffer, 0, BUFFER_SIZE);	//	REVIEW : 전체 탐색하는 것들은 성능 개선의 여지가 있음
 			bytesRead = read(connections, buffer, BUFFER_SIZE);
@@ -28,12 +29,17 @@ int main()
 			{	
 				std::cerr << "Could not read request." << std::endl;
 			}
-			std::cout << "BytesRead is : " << bytesRead << "\n";
+			else
+			{
+				std::cout << "BytesRead is : " << bytesRead << "\n";
+				request_buffer += std::string(buffer);
+			}
 		}
 		if (bytesRead != -1)
 		{
 		//	STUB : HttpMessageRequest
-			HttpMessageRequest	request(buffer);
+			// const char* req = request_buffer.c_str();
+			HttpMessageRequest	request((char*)request_buffer.c_str());
 			request.Parser();
 			HttpMessageResponse	response(request);
 			response.SetMessage();
